702ecb011d163aac44f2b232fcce6f9b5fa67b01
Daniel Carrasco Ortega <danielcarrascoortega@gmail.com>
2024-11-10 17:10:29 +0100

    Terminado el archivo notificaciones.js

--- notificaciones.js
+++ notificaciones.js
@@ -1,6 +1,8 @@
 /* Creo el archivo notificaciones.js en GitHub 
 
-Este script todavía no hace nada */
+Este script todavía no hace nada 
+
+Este script aunque no haga nada ya está finalizado */
 
 
 
